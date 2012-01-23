24af284fbfbbf90d9005c403f042bba4530bdb98
Tom Hicks <hickst@tohono.com>
2012-01-22 20:49:43 -0700

    Setup page size, orientation, and margins.

--- src/mayancal/genpdf.clj
+++ src/mayancal/genpdf.clj
@@ -1,12 +1,14 @@
 (ns mayancal.genpdf
-  (:import [com.itextpdf.text Document DocumentException Paragraph])
+  (:import [com.itextpdf.text Document DocumentException Element PageSize Paragraph])
   (:import [com.itextpdf.text.pdf PdfWriter]) )
 
 (defn genCal []
   "Top-level call to generate PDF version of Mayan Calendar"
-  (let [ document (Document.)
-        fos (java.io.FileOutputStream. "/tmp/firstPdf.pdf") ]
+  (let [ document (Document. (.rotate PageSize/LETTER))
+         fos (java.io.FileOutputStream. "/tmp/firstPdf.pdf") ]
     (PdfWriter/getInstance document fos)
+    (.setMargins document 72 72 108 72)
+    (.setMarginMirroringTopBottom document true) ;for top binding
     (.open document)
     (.add document (Paragraph. "Hello 2012!"))
     (.close document)))
