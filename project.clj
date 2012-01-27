1ab939c9dc288e10fc2eea9d7495339a9e892083
Tom Hicks <hickst@tohono.com>
2012-01-27 16:50:42 -0700

    Added core and command line.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject mayancal "1.0.0-SNAPSHOT"
   :description "Mayan calendar generator"
   :dependencies [ [org.clojure/clojure "1.3.0"]
+                  [org.clojure/tools.cli "0.2.1"]
                   [com.itextpdf/itextpdf "5.1.3"]
                   [com.itextpdf.tool/xmlworker "1.1.1"] ])
---
+++ src/mayancal/core.clj
@@ -0,0 +1,40 @@
+(ns mayancal.core
+  (:require [clojure.java.io :as io])
+  (:require [clojure.tools.cli :as cli])
+  (:require [mayancal.mcal :as mcal])
+  (:require [mayancal.genpdf :as pdf])
+  (:gen-class)
+)
+
+(defn -main [ & args]
+  (let [ usage "Usage: java -jar mayancal.jar -o out-pdf-file [year]"
+         [options other-args flag-usage]
+           (cli/cli args
+             ["-o" "The basename (without extension) for the output PDF file"]) ]
+
+    ;; if user asks for help, print usage messages and exit
+    (if (:h options)
+      (do
+        (println usage)
+        (println flag-usage)
+        (System/exit 1)))
+
+    ;; check for any missing arguments
+    (if (not (:o options))
+      (do
+        (println "ERROR: Required output file basename argument is missing.")
+        (println usage)
+        (println flag-usage)
+        (System/exit 2)))
+
+    ;; generate a round calendar the desired year and pass it to the PDF formatter
+    (let [ year (if (nil? (first other-args)) "2012" (first other-args))
+           roundcal (mcal/roundcal-for-year year) ]
+        (pdf/gen-cal roundcal (:o options)))
+))
+
+(comment
+  (ns mayancal.core)
+  (load "core")
+  (-main "-o" "out-file-basename")
+)
