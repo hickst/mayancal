0563e58fc9dfdc6e3e5f7ab0fd6bef48d64d4392
Tom Hicks <hickst@tohono.com>
2012-11-10 14:40:32 -0700

    Update to newest iText and CLI tools libs.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject mayancal "0.0.28"
   :description "Mayan calendar generator"
   :dependencies [ [org.clojure/clojure "1.4.0"]
-                  [org.clojure/tools.cli "0.2.1"]
-                  [com.itextpdf/itextpdf "5.1.3"]
-                  [com.itextpdf.tool/xmlworker "1.1.1"] ]
+                  [org.clojure/tools.cli "0.2.2"]
+                  [com.itextpdf/itextpdf "5.3.4"]
+                  [com.itextpdf.tool/xmlworker "1.2.1"] ]
   :main mayancal.core
   :uberjar-name "mayancal.jar"
 )
