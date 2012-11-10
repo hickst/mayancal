edc956928bac4f4cbe0dbb3d017e1295b0fc6b1b
Tom Hicks <hickst@tohono.com>
2012-11-10 14:09:13 -0700

    Move to Clojure 1.4.x. Redo version numbering.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject mayancal "1.0.0-SNAPSHOT"
+(defproject mayancal "0.0.28"
   :description "Mayan calendar generator"
-  :dependencies [ [org.clojure/clojure "1.3.0"]
+  :dependencies [ [org.clojure/clojure "1.4.0"]
                   [org.clojure/tools.cli "0.2.1"]
                   [com.itextpdf/itextpdf "5.1.3"]
                   [com.itextpdf.tool/xmlworker "1.1.1"] ]
