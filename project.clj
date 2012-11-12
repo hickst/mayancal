a56624bfe161aa35091ac1bbb7f205bb16553cf7
Tom Hicks <hickst@tohono.com>
2012-11-11 21:22:24 -0700

    Update app version and TODO list.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mayancal "0.0.28"
+(defproject mayancal "0.0.43"
   :description "Mayan calendar generator"
   :dependencies [ [org.clojure/clojure "1.4.0"]
                   [org.clojure/tools.cli "0.2.2"]
