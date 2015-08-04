d9f6e927647d119e2a932b0bfd76f4190f5ee422
Tom Hicks <hickst@tohono.com>
2015-08-04 10:46:28 -0700

    Upgrade to use clojure 1.6.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mayancal "1.0.0"
   :description "Mayan calendar generator"
-  :dependencies [ [org.clojure/clojure "1.4.0"]
+  :dependencies [ [org.clojure/clojure "1.6.0"]
                   [org.clojure/tools.cli "0.2.2"]
                   [com.itextpdf/itextpdf "5.3.4"]
                   [com.itextpdf.tool/xmlworker "1.2.1"] ]
